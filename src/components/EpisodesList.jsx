218c99c9b90db489447142f7b7cc0f34db5ed4c1
yalandr <52127093+yalandr@users.noreply.github.com>
2021-08-31 12:19:41 +0300

    Adding Pagination
    Pagination added on Episodes List

--- src/components/EpisodesList.jsx
+++ src/components/EpisodesList.jsx
@@ -1,10 +1,11 @@
-// import React from 'react';
 import React, {useEffect, useState} from 'react';
+import Pagination from './Pagination'
 
 function EpisodesList() {
 
     const [episodes, setEpisodes] = useState([]);
-
+    const [info, setInfo] = useState({});
+    
     const initialUrl = 'https://rickandmortyapi.com/api/episode';
 
     const fetchEpisodes = (url) => {
@@ -12,6 +13,7 @@ function EpisodesList() {
         .then(response => response.json())
         .then(data => {
             setEpisodes(data.results);
+            setInfo(data.info)
         })
         .catch(error => console.log(error))
     };
@@ -20,9 +22,23 @@ function EpisodesList() {
         fetchEpisodes(initialUrl);
     }, [])
 
+    const onPrevious = () => {
+        fetchEpisodes(info.prev)
+    }
+
+    const onNext = () => {
+        fetchEpisodes(info.next)
+    }
+
     return (
         <div className="episodes-list">
             <h1>Episodes list</h1>
+            <Pagination 
+                prev={info.prev} 
+                next={info.next}
+                onPrevious={onPrevious}
+                onNext={onNext} 
+            />
             {
                 episodes.map((item, index) => (
                     <div className="episodes-cards-container" key={index}>
@@ -40,6 +56,12 @@ function EpisodesList() {
                     </div>
                 ))
             }
+            <Pagination 
+                prev={info.prev} 
+                next={info.next}
+                onPrevious={onPrevious}
+                onNext={onNext} 
+            />
         </div>
     )
 }
---
+++ src/components/Pagination.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+
+
+const Pagination = ({prev, next, onPrevious, onNext}) => {
+
+    const handlePrevious = () => {
+        onPrevious();
+    }
+
+    const handleNext = () => {
+        onNext();
+    }
+
+    return (
+        <nav>
+            <ul className="pagination-list">
+                {
+                    prev ? (
+                    <li className="pagination-list-item">
+                        <button className="pagination-link" onClick={handlePrevious}>Previous</button>
+                    </li>
+                    ) : null
+                }
+                {
+                    next ? (
+                        <li className="pagination-list-item">
+                            <button className="pagination-link" onClick={handleNext}>Next</button>
+                        </li>
+                    ) : null
+                }
+                
+                
+            </ul>
+        </nav>
+    )
+}
+
+export default Pagination
+
